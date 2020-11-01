b2269071f68c2ec1e79cc4013cf09b713299f2c4
Roger Huba <rogerhuba@gmail.com>
2020-10-31 20:19:35 -0500

    add odd/even and number betting

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import random
 import sys
 from os import system
 
-balance, plays, red_bet_amount, green_bet_amount, black_bet_amount, odd_bet_amount, even_bet_amount = 0, 0, 0, 0, 0, 0, 0
+balance, plays, red_bet_amount, green_bet_amount, black_bet_amount, odd_bet_amount, even_bet_amount, number_bet_amount \
+    = 0, 0, 0, 0, 0, 0, 0, 0
 bet_red, bet_black, bet_green, bet_odd, bet_even = False, False, False, False, False
 bet_number, money_on_number, last_numbers, last_colors = [], [], [], []
 player_choice = ''
@@ -31,8 +32,12 @@ def spins():
     winning_color = slots[str(winning_number)]
     last_numbers.append(winning_number)
     last_colors.append(winning_color)
+    if winning_number %2 == 0:
+        winning_odd_even = 'even'
+    else:
+        winning_odd_even = 'odd'
     plays += 1
-    return winning_number, winning_color
+    return winning_number, winning_color, winning_odd_even
 
 
 def menu():
@@ -62,7 +67,7 @@ def adjust_balance():
 def print_screen():
     print(f'''
     Your current balance is {balance}.
-    Your current bet is {total_bet}
+    Your current bet is {total_bet}.
     You have played {plays} time(s).
     Current Red: {bet_red} | Current Black: {bet_black} | Current Green: {bet_green}
     Current Odd: {bet_odd} | Current Even: {bet_even}
@@ -76,11 +81,12 @@ def print_screen():
 
 def place_bet():
     global bet_red, bet_black, bet_odd, bet_even, bet_green, message, red_bet_amount, balance, total_bet, \
-        black_bet_amount, green_bet_amount
+        black_bet_amount, green_bet_amount, odd_bet_amount, even_bet_amount, number_bet_amount
     bet = ''
     while bet != 'q':
         screen_clear()
         print_screen()
+        # TODO: Need to fix the display to add color
         print(f'''
         1) Red  2) Black  3) Green
         4) Odd  5) Even
@@ -91,6 +97,7 @@ def place_bet():
         bet = input('Place your bet, 1-5: ')
         if bet == 'q':
             return
+        # TODO: This Code below can be made DRY!
         elif int(bet) == 1:
             if bet_red:
                 bet_red = False
@@ -145,13 +152,37 @@ def place_bet():
         elif int(bet) == 4:
             if bet_odd:
                 bet_odd = False
+                balance = int(balance) + int(odd_bet_amount)
+                total_bet = total_bet - int(odd_bet_amount)
+                odd_bet_amount = 0
             else:
-                bet_odd = True
+                odd_bet_amount = input('Please enter an amount to bet on ODD: ')
+                if int(odd_bet_amount) > int(balance):
+                    message = 'BET NOT PLACED! Balance to low.'
+                elif (int(odd_bet_amount) + int(total_bet)) > max_bet:
+                    message = 'BET NOT PLACED! Exceeded Max bet'
+                else:
+                    total_bet += int(odd_bet_amount)
+                    balance = int(balance) - int(odd_bet_amount)
+                    bet_odd = True
+                    message = 'Bet placed on ODD'
         elif int(bet) == 5:
             if bet_even:
                 bet_even = False
+                balance = int(balance) + int(even_bet_amount)
+                total_bet = total_bet - int(even_bet_amount)
+                even_bet_amount = 0
             else:
-                bet_even = True
+                even_bet_amount = input('Please enter an amount to bet on EVEN: ')
+                if int(even_bet_amount) > int(balance):
+                    message = 'BET NOT PLACED! Balance to low.'
+                elif (int(even_bet_amount) + int(total_bet)) > max_bet:
+                    message = 'BET NOT PLACED! Exceeded Max bet'
+                else:
+                    total_bet += int(even_bet_amount)
+                    balance = int(balance) - int(even_bet_amount)
+                    bet_even = True
+                    message = 'Bet placed on EVEN'
         elif int(bet) == 6:
             number_to_add = int(input('Enter a number 1-36: '))
             while 1 > number_to_add > 36:
@@ -159,7 +190,15 @@ def place_bet():
             if number_to_add in bet_number:
                 bet_number.remove(number_to_add)
             else:
-                bet_number.append(number_to_add)
+                number_bet_amount = input(f'Please enter an amount to bet on {number_to_add}: ')
+                if int(number_bet_amount) > int(balance):
+                    message = 'BET NOT PLACED! Balance to low.'
+                elif (int(number_bet_amount) + int(total_bet)) > max_bet:
+                    message = 'BET NOT PLACED! Exceeded Max bet'
+                else:
+                    total_bet += int(number_bet_amount)
+                    balance = int(balance) - int(number_bet_amount)
+                    message = f'Bet placed on {number_to_add}!'
         elif int(bet) == 7:
             message = 'Group betting not yet implemented!'
         else:
@@ -172,14 +211,25 @@ def play_game():
         bet_even, black_bet_amount, odd_bet_amount, even_bet_amount, message
     message = ''
     winning_return = spins()
-    win_number, win_color = winning_return[0], winning_return[1]
+    win_number, win_color, odd_even = winning_return[0], winning_return[1], winning_return[2]
     print('Win Color is: ' + win_color)
+    # TODO: May need to do a check on the bet and then drill down
+    # TODO: This function is doing to much.  Make a function for changing balances.
     if win_color is 'red' and bet_red:
         balance = int(balance) + (int(red_bet_amount) * 2)
         message = 'You won on RED '
     if win_color is 'black' and bet_black:
         balance = int(balance) + (int(black_bet_amount) * 2)
         message += 'You won on BLACK'
+    if odd_even is 'even' and bet_even:
+        balance = int(balance) + (int(even_bet_amount) * 2)
+        message += 'You won on EVEN'
+    if odd_even is 'odd' and bet_odd:
+        balance = int(balance) + (int(odd_bet_amount) * 2)
+        message += 'You won on ODD'
+    if win_number in bet_number:
+        balance = int(balance) + (int(number_bet_amount) * 35)
+    # TODO: Add these individually to each win condition.
     red_bet_amount, black_bet_amount, odd_bet_amount, even_bet_amount, total_bet = 0, 0, 0, 0, 0
     bet_red, bet_black, bet_green, bet_odd, bet_even = False, False, False, False, False
     return
@@ -187,6 +237,7 @@ def play_game():
 
 if __name__ == '__main__':
     screen_clear()
+    # TODO: This can be cleaned up a little!
     while not player_choice:
         player_choice = menu()
         if not player_choice:
